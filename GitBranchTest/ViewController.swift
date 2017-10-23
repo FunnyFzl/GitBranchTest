89de62bee21e23379ff4c9b743c1e9974e2fad01
fengzl <fengzl@youyoubaoxian.com>
2017-10-23 14:56:46 +0800

    first commit

---
+++ GitBranchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitBranchTest
+//
+//  Created by Funny on 2017/10/23.
+//  Copyright © 2017年 Funny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
